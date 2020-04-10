54147c715b5be6a334aa78c4db5170f38c25d674
shurawz <xuchepran9@gmail.com>
2020-04-10 15:21:09 +0545

    Added some comments to make it clear.

--- ModulesAndFunctions/Time/timeExample.py
+++ ModulesAndFunctions/Time/timeExample.py
@@ -1,11 +1,11 @@
 ######First time on time
 
-# import time
-#
-# print(time.gmtime(0))
-# print(time.localtime())
-#
-# print(time.time())
+import time
+
+print(time.gmtime(0))
+print(time.localtime())
+
+print(time.time())  #Total number of seconds from the first second 1970 epoch.
 
 
 ######Second time
@@ -21,21 +21,21 @@
 
 ######Third time and hard one
 
-import time
-from time import time as my_timer
-import random
-
-input("Please enter to start")
-
-wait_time = random.randint(1, 6)
-time.sleep(wait_time)
-start_time = my_timer()
-
-input("Please enter to end")
-
-end_time = my_timer()
-
-print("Started at: " + time.strftime("%X", time.localtime(start_time)))
-print("Ended at: " + time.strftime("%X", time.localtime(end_time)))
-
-print("Your reaction time is {0} seconds.".format(end_time - start_time))
+# import time
+# from time import time as my_timer
+# import random
+#
+# input("Please enter to start")
+#
+# wait_time = random.randint(1, 6)
+# time.sleep(wait_time)
+# start_time = my_timer()
+#
+# input("Please enter to end")
+#
+# end_time = my_timer()
+#
+# print("Started at: " + time.strftime("%X", time.localtime(start_time)))
+# print("Ended at: " + time.strftime("%X", time.localtime(end_time)))
+#
+# print("Your reaction time is {0} seconds.".format(end_time - start_time))
