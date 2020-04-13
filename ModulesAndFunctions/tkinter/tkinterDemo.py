9d22d8d190017a0be3dc1a2e131764bfd0cab3dd
shurawz <xuchepran9@gmail.com>
2020-04-13 15:47:56 +0545

    Added some details commentation

--- ModulesAndFunctions/tkinter/tkinterDemo.py
+++ ModulesAndFunctions/tkinter/tkinterDemo.py
@@ -12,7 +12,7 @@ mainWindow = tkinter.Tk()
 mainWindow.title("Tkinter tutorial")
 mainWindow.geometry('640x480')
 
-#If line isnot in the program, the program won't run at the moment.
+#If line 20 isnot in the program, the program won't run at the moment.
 #The reason behind this scenario is:
 #We need to pass control over to Tk by calling main method on our top window because main method handles the -->
 # --> event processing that the GUI needs in order to function so we actually need add this code
