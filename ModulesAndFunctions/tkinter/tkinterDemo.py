5cd133e85d688b52526d60dc3e127566bff73d81
shurawz <xuchepran9@gmail.com>
2020-04-13 15:46:20 +0545

    Beginning on Tkinter and also created links note

---
+++ ModulesAndFunctions/tkinter/tkinterDemo.py
@@ -0,0 +1,20 @@
+try:
+    import tkinter
+except ImportError:
+    import Tkinter as tkinter
+
+print(tkinter.TkVersion)
+print(tkinter.TclVersion)
+
+
+mainWindow = tkinter.Tk()
+
+mainWindow.title("Tkinter tutorial")
+mainWindow.geometry('640x480')
+
+#If line isnot in the program, the program won't run at the moment.
+#The reason behind this scenario is:
+#We need to pass control over to Tk by calling main method on our top window because main method handles the -->
+# --> event processing that the GUI needs in order to function so we actually need add this code
+
+mainWindow.mainloop()
