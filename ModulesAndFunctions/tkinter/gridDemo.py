d5857efc122c6847a124d08e979bf5f5051093cb
shurawz <xuchepran9@gmail.com>
2020-04-14 15:25:54 +0545

    Demo program on the grid

---
+++ ModulesAndFunctions/tkinter/gridDemo.py
@@ -0,0 +1,45 @@
+import tkinter
+
+mainWindow = tkinter.Tk()
+
+mainWindow.title("Hello World")
+mainWindow.geometry('640x480-8-200')
+
+label = tkinter.Label(mainWindow, text="Hello Bhai")
+label.grid(row=0, column=0)
+
+leftFrame = tkinter.Frame(mainWindow)
+leftFrame.grid(row=1, column=1)
+
+canvas = tkinter.Canvas(leftFrame, relief='raised', borderwidth=1)
+canvas.grid(row=1, column=0)     # It fills the left side of window with canvas leaving labeled part.
+
+rightFrame = tkinter.Frame(mainWindow)
+rightFrame.grid(row=1, column=2, sticky='n')
+
+button1 = tkinter.Button(rightFrame, text="Button1")
+button2 = tkinter.Button(rightFrame, text="Button2")
+button3 = tkinter.Button(rightFrame, text="Button3")
+button4 = tkinter.Button(rightFrame, text="Button4")
+
+button1.grid(row=0, column=0)
+button2.grid(row=1, column=0)
+button3.grid(row=2, column=0)
+button4.grid(row=3, column=0)
+
+# Below code will maintain the gaps between the different segments or blocks.
+# Configure the columns
+mainWindow.columnconfigure(0, weight=1)
+mainWindow.columnconfigure(1, weight=1)
+mainWindow.grid_columnconfigure(2, weight=1) # 'columnconfigure' and 'grid_columnconfigure' means the same thing.
+
+leftFrame.config(relief='sunken', borderwidth=1)
+rightFrame.config(relief='sunken', borderwidth=1)
+leftFrame.grid(sticky='ns')
+rightFrame.grid(sticky='new')
+
+rightFrame.columnconfigure(0, weight=1)
+button2.grid(sticky='ew')
+button3.grid(sticky='ew')
+
+mainWindow.mainloop()
