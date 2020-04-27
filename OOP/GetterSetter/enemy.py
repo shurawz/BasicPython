404d7ce52a21c123222ce52010069d3dde706279
shurawz <xuchepran9@gmail.com>
2020-04-27 15:57:31 +0545

    Some lines of code are added and also gone for the challenge

--- OOP/GetterSetter/enemy.py
+++ OOP/GetterSetter/enemy.py
@@ -1,22 +1,65 @@
 class Enemy:  # or we can write class Enemy(object) both mean same
 
     def __init__(self, name="Gotame", hit_points=0, lives=1):
-        self.name = name
-        self.hit_points = hit_points
-        self.lives = lives
+        self._name = name
+        self._hit_points = hit_points
+        self._lives = lives
+        self._alive = True
 
     def take_damage(self, damage):
-        remaining_point = self.hit_points - damage
+        remaining_point = self._hit_points - damage
         if remaining_point >= 0:
-            self.hit_points = remaining_point
-            print("I took {} points damage and have {} left.".format(damage, self.hit_points))
+            self._hit_points = remaining_point
+            print("I took {} points damage and have {} left.".format(damage, self._hit_points))
         else:
-            self.lives -= 1
-            self.hit_points = 0
+            self._lives -= 1
+            if self._lives > 0:
+                print("{0._name}, you have {0._lives} life left.".format(self))
+            else:
+                print("{0._name}, you're dead now.".format(self))
+                self._alive = False
 
     def __str__(self):
-        return "Name: {0.name}, Hit Points: {0.hit_points}, Lives: {0.lives}.".format(self)
+        return "Name: {0._name}, Hit Points: {0._hit_points}, Lives: {0._lives}.".format(self)
+
 
 class Troll(Enemy):
-    pass  # In Python, pass keyword is used to execute nothing; it means, when we don't want to execute code,
-  # the pass can be used to execute empty.
+    # In Python, pass keyword is used to execute nothing; it means, when we don't want to execute code,
+    # the pass can be used to execute empty.
+
+    
+    def __init__(self, name):
+        super().__init__(name=name, lives=1, hit_points=23)
+
+    def test(self):
+        print("I am {0._name}".format(self))
+
+
+class Vampyre(Enemy):
+
+    def __init__(self, name):
+        super().__init__(name=name, lives=3, hit_points=12)
+
+    def dodges(self):
+        import random
+        if random.randint(1, 3) == 3:
+            print("**** {0._name} dodges ****".format(self))
+            return True
+        else:
+            return False
+
+    def take_damage(self, damage):
+        if not self.dodges():
+            super().take_damage(damage=damage)
+
+
+# Challenge part...
+class VampyreKing(Vampyre):
+
+    def __init__(self, name):
+        super().__init__(name=name)
+        self._hit_points = 140
+        self._lives = 14
+
+    def take_damage(self, damage):
+        super().take_damage(damage // 4)
--- OOP/GetterSetter/enemyMain.py
+++ OOP/GetterSetter/enemyMain.py
@@ -1,10 +1,43 @@
-from enemy import Enemy, Troll
+from enemy import Enemy, Troll, Vampyre, VampyreKing
+#
 
-ugly = Troll()
-print("Ugly = {}".format(ugly))
-
-ugl = Troll("Maatokni", 13, 4)
-print("Ugl = {}".format(ugl))
-
-uzi = Troll("Gun", 8)
-print(uzi)
+dracula = VampyreKing("Something")
+print(dracula)
+dracula.take_damage(12)
+print(dracula)
+# ugly = Troll("Guys")
+# print(ugly)
+#
+# ugl = Troll("Maatokni")
+# print(ugl)
+#
+# uzi = Troll("Gun")
+# print(uzi)
+#
+# ump45 = Vampyre("SMG")
+# print(ump45)
+#
+# m416 = Vampyre("Assaulter")
+# print(m416)
+#
+# uzi.take_damage(8)
+# m416.take_damage(13)
+# print(m416)
+#
+# # while m416.alive:
+# #     m416.take_damage(6)
+# #     print(m416)
+#
+# # while m416.alive:
+# #     if not m416.dodges():
+# #         m416.take_damage(1)
+# #         print(m416)
+#
+# while m416._alive:
+#     m416.take_damage(6)
+#     # print(m416)
+#
+# m416._lives = 0
+# m416._hit_points = 1
+# print(m416)
+#
