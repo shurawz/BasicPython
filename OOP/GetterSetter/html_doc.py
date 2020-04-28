c535b80bc573308366f0b04162557ec32ead513f
shurawz <xuchepran9@gmail.com>
2020-04-28 15:40:17 +0545

    Additional concept on Composition and new on Aggregation

--- OOP/GetterSetter/html_doc.py
+++ OOP/GetterSetter/html_doc.py
@@ -1,8 +1,13 @@
+""" Composition means that an object knows another object, and explicitly delegates some tasks to it.
+    While inheritance is implicit, composition is explicit: in Python, however, things are far more interesting
+    than this =). The primary goal of composition is to relax the coupling between objects."""
+
+
 class Tag:
 
     def __init__(self, name, content):
-        self.start_tag = '<{}'.format(name)
-        self.end_tag = '<{}'.format(name)
+        self.start_tag = '<{}>'.format(name)
+        self.end_tag = '</{}>'.format(name)
         self.content = content
 
     def __str__(self):
@@ -21,8 +26,11 @@ class DocType(Tag):
 
 class Head(Tag):
 
-    def __init__(self):
+    def __init__(self, title=None):
         super().__init__('head', '')
+        if title:
+            self._title_tag = Tag('title', title)
+            self.content = str(self._title_tag)
 
 
 class Body(Tag):
@@ -44,25 +52,66 @@ class Body(Tag):
 
 class HtmlDoc(object):
 
-    def __init__(self):
-        self._body = Body()
-        self._head = Head()
-        self._docType = DocType()
+    def __init__(self, doc_type, head, body):
+        self._body = body
+        self._head = head
+        self._doc_type = doc_type
 
     def add_tag(self, name, content):
         self._body.add_tag(name, content)
 
     def display(self, file=None):
-        self._docType.display(file=file)
+        self._doc_type.display(file=file)
         print("<html>", file=file)
         self._head.display(file=file)
         self._body.display(file=file)
         print("</html>", file=file)
 
 
+# if __name__ == '__main__':
+#     myPage = HtmlDoc('Demo HTML Document')
+#     myPage.add_tag('title', 'Learning Phase')
+#     myPage.add_tag('h1', 'Main Heading')
+#     myPage.add_tag('h2', 'Sub-main Heading')
+#     myPage.add_tag('p', 'This is the content that is being displayed on the screen.')
+#     with open('demo.html', 'w') as text_doc:
+#         myPage.display(text_doc)
+#
+# # Here, the content of the head remains same but the content of the body gets changed as I have created new body.
+# new_body = Body()
+# new_body.add_tag('h1', 'Aggregation')
+# new_body.add_tag('p', "<strong>Ek pal</strong> kaa jeena fir to heh jaana"
+#                      " - Toofa to leke jaiye kudiyo ko dekhaana.")
+# new_body.add_tag('p', "Dil leke darde dil dey gaye, tum jaan jaan khehke mere jaan le gaye.")
+#
+#
+# myPage._body = new_body
+# with open("demo2.html", 'w') as text_doc:
+#     myPage.display(text_doc)
+
 if __name__ == '__main__':
-    myPage = HtmlDoc()
-    myPage.add_tag('h1', 'Main Heading')
-    myPage.add_tag('h2', 'Sub-main Heading')
-    myPage.add_tag('p', 'This is the content that is being displayed on the screen.')
-    myPage.display()
+    # myPage = HtmlDoc('Demo HTML Document')
+    # myPage.add_tag('title', 'Learning Phase')
+    # myPage.add_tag('h1', 'Main Heading')
+    # myPage.add_tag('h2', 'Sub-main Heading')
+    # myPage.add_tag('p', 'This is the content that is being displayed on the screen.')
+    # with open('demo.html', 'w') as text_doc:
+    #     myPage.display(text_doc)
+
+# Here, the content of the head remains same but the content of the body gets changed as I have created new body.
+    new_body = Body()
+    new_body.add_tag('h1', 'Aggregation')
+    new_body.add_tag('p', "<strong>Ek pal</strong> kaa jeena fir to heh jaana"
+                          " - Toofa to leke jaiye kudiyo ko dekhaana.")
+    new_body.add_tag('p', "Dil leke darde dil dey gaye, tum jaan jaan khehke mere jaan le gaye.")
+
+
+    new_docType = DocType()
+    new_header = Head('Aggregation Segment')
+    myPage = HtmlDoc(new_docType, new_header, new_body)
+
+    with open("demo2.html", 'w') as text_doc:
+        myPage.display(text_doc)
+
+"""Aggregation is a week form of composition. If you delete the container object contents objects can live without 
+   container object."""
