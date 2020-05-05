837b6599456950aeafc00e59e284e72d78294f6b
shurawz <xuchepran9@gmail.com>
2020-05-05 16:06:44 +0545

    Still more to learn dear.

--- DataBase/RollingBack/checkdb.py
+++ DataBase/RollingBack/checkdb.py
@@ -1,12 +1,13 @@
 import sqlite3
+import pytz
 
 db = sqlite3.connect('accounting.sqlite', detect_types=sqlite3.PARSE_DECLTYPES)
 
 
 for row in db.execute("SELECT * FROM history"):
     # print(row)
-
-    local_time = row[0]
-    print("{}\t{}.".format(local_time, type(local_time)))
+    utc_time = row[0]
+    local_time = pytz.utc.localize(utc_time).astimezone()
+    print("{}\t{}.".format(utc_time, local_time))
 
 db.close()
